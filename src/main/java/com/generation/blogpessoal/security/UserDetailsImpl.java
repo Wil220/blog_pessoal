9d8eba574fc83efc3761d2a2d7454708264d2d8d
Wil220 <wilker.silva0714@gmail.com>
2024-07-18 23:23:27 -0300

    basic security

--- src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
+++ src/main/java/com/generation/blogpessoal/security/UserDetailsImpl.java
@@ -1,5 +1,72 @@
 package com.generation.blogpessoal.security;
 
-public class UserDetailsImpl {
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.generation.blogpessoal.model.Usuario;
+
+public class UserDetailsImpl implements UserDetails{
+	
+private static final long serialVersionUID = 1L;
+
+private String userName;
+private String password;
+private List<GrantedAuthority> authorities;
+
+public UserDetailsImpl(Usuario user) {
+	this.setUserName(user.getUsuario());
+	this.setPassword(user.getSenha());
+}
+public UserDetailsImpl() { }
+
+@Override
+public Collection<? extends GrantedAuthority> getAuthorities() {
+	
+	return null;
+}
+@Override
+public String getPassword() {
+	
+	return null;
+}
+@Override
+public String getUsername() {
+	
+	return null;
+}
+@Override
+public boolean isAccountNonExpired() {
+	return true;
+}
+
+@Override
+public boolean isAccountNonLocked() {
+	return true;
+}
+
+@Override
+public boolean isCredentialsNonExpired() {
+	return true;
+}
+
+@Override
+public boolean isEnabled() {
+	return true;
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+public void setAuthorities(List<GrantedAuthority> authorities) {
+	this.authorities = authorities;
+}
 
 }
