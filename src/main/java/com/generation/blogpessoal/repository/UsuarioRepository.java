1c3b9d40db6e9f90f615a48999a8c96d0672bded
Wil220 <wilker.silva0714@gmail.com>
2024-07-18 23:16:28 -0300

    classe service

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -9,5 +9,4 @@ import com.generation.blogpessoal.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	public Optional<Usuario> findByUsuario(String usuario);
-	
 }
